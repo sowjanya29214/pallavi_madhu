2a76eb691244bca197f1175a70e8757fd523e6f0
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:07:26 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+sowjanya=["pallavi","sowjanya","navya","divya","bindu","renuka"]
+print(sowjanya)
+
+pallavi=["parrot","peacock","indian roller","pegion","crow","hen","sparraow"]
+print(pallavi[3:])
+
+swarupa=["lily","hibiscus","rose","gorinta","marrigold","sunflower","chamanthi"]
+print(swarupa[-6:-4])
+
+madhu=["custard apple","mango","guava","jackfruit","apple","grapes","watermelon"]
+if "guava" in madhu:
+    print("yes, 'guava' is in the fruits list")
+    print("no, 'banana' is in the fruits list")
+
+    
