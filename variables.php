a02feedb99383bfd938bbb1d6c6317c041b06e14
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:18:33 +0530

    variables.php

---
+++ variables.php
@@ -0,0 +1,25 @@
+<?php
+$txt1 ="MY FAMILY";
+$txt2 ="MADHU";
+$txt3 ="SWARUPA";
+$txt4 ="AKHILA";
+$txt5 ="SOWJANYA";
+$x="29";
+$y="14";
+$z="21";
+echo$txt1;
+echo"<br>";
+echo$txt2;
+echo"<br>";
+echo$txt3;
+echo"<br>";
+echo$txt4;
+echo"<br>";
+echo$txt5;
+echo"<br>";
+echo$x;
+echo"<br>";
+echo$y;
+echo"<br>";
+echo$z;
+?>
