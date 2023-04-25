8f1a1e00061abc5705ea7c2a46067cae57b61898
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:11:28 +0530

    Dt array.php

---
+++ Dt array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $flowers=array("rose","lilly","marigold","gorinta");
+    var_dump($flowers);
+    echo "<br>";
+    $choclates=array("5star","kitkat","dirymilk","snikers");
+    var_dump($choclates);
+    ?>
+</body>
+</html>
