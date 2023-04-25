0430adf3a04ac3da7aceac991cc80a73d8886f57
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:17:02 +0530

    switch .php

---
+++ switch .php
@@ -0,0 +1,16 @@
+<?php
+$favchoclate="5star";
+switch ($favchoclate) {
+    case "5star":
+        echo "your favourite choclate is 5star";
+        break;
+    case "snikers":
+        echo "your favourite choclate is snikers";
+        break;
+    case "dairymilk":
+        echo "your favourite choclate is dairymilk";
+        break;
+    default:
+        echo "your favourite choclate is neither 5star,snikers, nor dairymilk";
+    }
+?>
