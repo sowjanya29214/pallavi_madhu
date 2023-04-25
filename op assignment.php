2f2e898fad887fee9807b3f88eff95dc4d7bbe9e
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:16:36 +0530

    op assignment.php

---
+++ op assignment.php
@@ -0,0 +1,41 @@
+<h3>equal</h3>
+<?php
+$r =80;
+echo $r ;
+echo"<br>";
+?>
+<h3>addition</h3>
+<?php
+$p=10;
+$r +=80;
+echo $r ;
+echo"<br>";
+?>
+<h3>substraction</h3>
+<?php
+$p=150;
+$r -=80;
+echo $p;
+echo"<br>";
+?>
+<h3>multiplication</h3>
+<?php
+$q=20;
+$r *=80;
+echo $p;
+echo"<br>";
+?>
+<h3>division</h3>
+<?php
+$p=150;
+$r /=80;
+echo $q;
+echo"<br>";
+?>
+<h3>modulus</h3>
+<?php
+$p %=150;
+$q=20;
+echo $p ;
+echo"<br>";
+?>
