f1f662bf4a0d241fe1faa19a7700e9009fa9440b
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:14:45 +0530

    if...else.php

---
+++ if...else.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible=" IE=edge">
+    <meta name="viewport" content="width" , initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $x = date("H");
+    if ($x > "40") {
+        echo "navya";
+    }
+     else {
+        echo "sowjanya";
+    }
+    ?>
+</body>
+
+</html>
