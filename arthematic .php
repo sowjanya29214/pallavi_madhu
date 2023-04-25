fef94db98e9a4d2f2ddb770b590c8ed074c66550
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:09:16 +0530

    arthematic.php

---
+++ arthematic .php
@@ -0,0 +1,44 @@
+<?php
+$a=20;
+$b=30;
+$c=50;
+echo $a + $b + $c;
+echo"<br>";
+?>
+<h3>substraction</h3>
+<?php
+$p=150;
+$q=20;
+$r=80;
+echo $p - $q - $r;
+echo"<br>";
+?>
+<h3>multiplication</h3>
+<?php
+$x=2;
+$y=3;
+$z=4;
+echo $x * $y * $z;
+echo"<br>";
+?>
+<h3>Division</h3>
+<?php
+$a=90;
+$b=32;
+echo $x / $b;
+?>
+<h3>modulus</h3>
+<?php
+$e=9;
+$f=3;
+$g=6;
+echo $e % $f % $g;
+echo"<br>";
+?>
+<h3>Exponentiation</h3>
+<?php
+$e=9;
+$f=3;
+echo $e ** $f ;
+echo"<br>";
+?>
