57b38f11620695dc74bf6a415f7178071cfaf359
sowjanya29214 <131737979+sowjanya29214@users.noreply.github.com>
2023-04-25 22:10:29 +0530

    const.php

---
+++ const array.php
@@ -0,0 +1,7 @@
+<?php
+define("movies",["ARYA","DESHAMMUDURU","PUSHPA"]);
+echo movies[1];
+echo"<br>";
+define("songs", ["ado priya ragam","antantodey","nv ammay ammy antantey"]);
+echo songs[1];
+?>
